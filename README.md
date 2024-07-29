ff76bb9e94b3c4a14b035dee1d57f73d2c3c8e65
hja03 <harvey7ayling@gmail.com>
2024-07-29 09:49:18 +0100

    Removed PyCache and added doc

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Representative Graph Sampling
 
 
+## Installation Instructions
+1. Requires ```Python 3.11```
+2. ```pip install -r requirements.txt```
+3. ```pip install -e .```
+
+
 ## To-Do
 - [x] **Implement Jaccard Similarity**. For comparing diversity of samples.
 
--- rep_graph_sample/dataset.py
+++ rep_graph_sample/dataset.py
@@ -14,8 +14,6 @@ def ogb_dataset_to_nx_graph(dataset_name: str) -> nx.Graph:
         dataset = LinkPropPredDataset(name=dataset_name, root='./dataset')
         graph = dataset[0]
 
-        print(graph)
-
     else:
         raise NotImplementedError(f'Dataset name: {dataset_name} not supported!')
 
@@ -25,6 +23,8 @@ def ogb_dataset_to_nx_graph(dataset_name: str) -> nx.Graph:
     if graph['node_feat'] is not None:
         node_features = {i:feature for i, feature in enumerate(graph['node_feat'])}
         nx.set_node_attributes(nx_graph, node_features, name='feature')
+    else:
+        print('INFO: Dataset does not contain node features or they could not be found.')
 
     assert(nx_graph.number_of_nodes() == graph['num_nodes'])
 
