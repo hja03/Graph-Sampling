941ee62ebb83584c9f51489cfc18b64ef6824f20
Harvey Ayling <oxfd2710@arc-login03.arcus-htc.arc.local>
2024-07-29 14:25:58 +0100

    Arc script tweaks

--- arc_scripts/mh.sh
+++ arc_scripts/mh.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=00:20:00
-#SBATCH --partition=short
+#SBATCH --partition=medium
+#SBATCH --clusters=htc
+#SBATCH --cpus-per-task=1
 
-
+echo 'Loading Python'
 module load Python/3.11.3-GCCcore-12.3.0
+
+echo 'Activating venv'
 source ../venv/bin/activate
-pip list
-python mh.py
+
+echo 'Running Script'
+python mh.py --iters 200000 --exponent 500
 deactivate
+
+echo ' --- Done --- '
